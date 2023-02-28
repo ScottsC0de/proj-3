784048ad9ed3335d7cfadb16b428851101d6948d
David Proto <davidproto@sbcglobal.net>
2023-02-28 18:08:39 -0500

    added app.js .env and fuctionality to flickr api calls

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+function FlickrPhotos() {
+  const [photos, setPhotos] = useState([]);
 
-function App() {
+  useEffect(() => {
+    async function fetchPhotos() {
+      const apiKey = process.env.REACT_APP_API_KEY;
+      const Tags = "nature";
+      const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${Tags}&format=json&nojsoncallback=1`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setPhotos(data.photos.photo);
+      console.log(data.photos.photo);
+    }
+    fetchPhotos();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {photos.map((photo) => (
+        <img
+          key={photo.id}
+          src={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+          alt={photo.title}
+        />
+      ))}
     </div>
   );
 }
 
-export default App;
+export default FlickrPhotos;
