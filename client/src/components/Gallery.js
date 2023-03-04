7e909ec0662702c3cc2c7b5141576e6248727ece
David Proto <davidproto@sbcglobal.net>
2023-03-04 14:19:17 -0500

    added functionality to cloud.js

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,62 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { CloudinaryContext, Transformation, Image } from 'cloudinary-react';
-import { render } from 'react-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { CloudinaryContext, Transformation, Image } from "cloudinary-react";
+import { render } from "react-dom";
 
 function Gallery() {
-    const [gallery, setGallery] = useState([]);
+  const [gallery, setGallery] = useState([]);
 
-  
-    useEffect(() => {
-        async function fetchGallery() {
-        // Request for images      
-        axios.get('https://res.cloudinary.com/demo/image/list.json')
-            .then(res => {
-                console.log(res.data.resources);
-                setGallery({gallery: res.data.resources});
-            });
+  useEffect(() => {
+    async function fetchGallery() {
+      // Request for images
+      axios
+        .get("https://res.cloudinary.com/demo/image/list.json")
+        .then((res) => {
+          console.log(res.data.resources);
+          setGallery({ gallery: res.data.resources });
+        });
     }
     fetchGallery();
-    }, []);
+  }, []);
 
-    // uploadWidget() {
-    //    // . . .
-    // }
-        return (
-            <div className="main">
-                <h1>Gallery</h1>
-                <div className="gallery">
-                    <CloudinaryContext cloudName="cloud_name">
-                        {
-                            gallery.map(data => {
-                                return (
-                                    <div className="responsive" key={data.public_id}>
-                                        <div className="img">
-                                            <a target="_blank" href={`https://res.cloudinary.com/demo/image/upload/${data.public_id}.jpg`}>
-                                                <Image publicId={data.public_id}>
-                                                    <Transformation
-                                                        crop="scale"
-                                                        width="300"
-                                                        height="200"
-                                                        dpr="auto"
-                                                        responsive_placeholder="blank"
-                                                    />
-                                                </Image>
-                                            </a>
-                                            <div className="desc">Created at {data.created_at}</div>
-                                        </div>
-                                    </div>
-                                )
-                            })
-                        }
-                    </CloudinaryContext>
-                    <div className="clearfix"></div>
+  // uploadWidget() {
+  //    // . . .
+  // }
+  return (
+    <div className="main">
+      <h1>Gallery</h1>
+      <div className="gallery">
+        <CloudinaryContext cloudName="cloud_name">
+          {gallery.map((data) => {
+            return (
+              <div className="responsive" key={data.public_id}>
+                <div className="img">
+                  <a
+                    target="_blank"
+                    href={`https://res.cloudinary.com/demo/image/upload/${data.public_id}.jpg`}
+                  >
+                    <Image publicId={data.public_id}>
+                      <Transformation
+                        crop="scale"
+                        width="300"
+                        height="200"
+                        dpr="auto"
+                        responsive_placeholder="blank"
+                      />
+                    </Image>
+                  </a>
+                  <div className="desc">Created at {data.created_at}</div>
                 </div>
-            </div>
+              </div>
+            );
+          })}
+        </CloudinaryContext>
+        <div className="clearfix"></div>
+      </div>
+    </div>
+  );
+}
 
-        );
-    }
-
-
-    export default Gallery;
-
+export default Gallery;
--- client/src/components/cloud.js
+++ client/src/components/cloud.js
@@ -1,27 +1,65 @@
-import React from "react";
-import { Cloudinary } from "@cloudinary/url-gen";
-import { AdvancedImage } from "@cloudinary/react";
-import { fill } from "@cloudinary/url-gen/actions/resize";
+import React, { useState, useEffect } from "react";
+import { CloudinaryContext, Image, Video } from "cloudinary-react";
+import { Cloudinary } from "@cloudinary/base";
+
+const cloudinary = new Cloudinary({
+  cloud: {
+    cloudName: "dtsv4ybqe",
+  },
+});
+
+const searchImages = async (query) => {
+  const { resources } = await cloudinary.search().expression(query).execute();
+
+  return resources;
+};
+
+const Search = () => {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      const resources = await searchImages(query);
+      setResults(resources);
+    };
+
+    fetchResults();
+  }, [query]);
+
+  const handleQueryChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  return (
+    <CloudinaryContext cloudName="dtsv4ybqe">
+      <div>
+        <label htmlFor="search-input">Search for images:</label>
+        <input
+          type="text"
+          id="search-input"
+          value={query}
+          onChange={handleQueryChange}
+        />
+      </div>
+      <div>
+        {results.map((resource) => (
+          <div key={resource.public_id}>
+            <Image publicId={resource.public_id} />
+          </div>
+        ))}
+      </div>
+    </CloudinaryContext>
+  );
+};
 
 const cloud = () => {
-  // Create a Cloudinary instance and set your cloud name.
-  const cld = new Cloudinary({
-    cloud: {
-      cloudName: "demo",
-    },
-  });
-
-  // Instantiate a CloudinaryImage object for the image with the public ID, 'docs/models'.
-  const myImage = cld.image("docs/models");
-
-  // Resize to 250 x 250 pixels using the 'fill' crop mode.
-  myImage.resize(fill().width(250).height(250));
-
-  // Render the image in a React component.
   return (
     <div>
-      <AdvancedImage cldImg={myImage} />
+      <h1>Search for Images</h1>
+      <Search />
     </div>
   );
 };
+
 export default cloud;
