b4e17f6c85224d1e9140feb95aa6757650f5117e
lynnadelesadler <lynnadelesadler@yahoo.com>
2023-03-03 19:15:36 -0500

    attempt to resolve cloudinary issues

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -4,59 +4,35 @@ import { CloudinaryContext, Transformation, Image } from 'cloudinary-react';
 import { render } from 'react-dom';
 
 function Gallery() {
-    const [gallery, setGallery] = useState([]);
+  const [gallery, setGallery] = useState([]);
 
-  
-    useEffect(() => {
-        async function fetchGallery() {
-        // Request for images      
-        axios.get('https://res.cloudinary.com/demo/image/list.json')
-            .then(res => {
-                console.log(res.data.resources);
-                setGallery({gallery: res.data.resources});
-            });
-    }
+
+  useEffect(() => {
+    async function fetchGallery() {
+      // Request for images      
+      const apiKey = process.env.API_KEY;
+      const apiSecret = process.env.API_SECRET;
+      const cloudName = process.env.CLOUD_NAME;
+      const url = `https://${apiKey}:${apiSecret}@api.cloudinary.com/v1_1/${cloudName}/resources/image`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setGallery(data.images.resources);
+      console.log(data.images.resources);
+    };
     fetchGallery();
-    }, []);
+  }, []);
 
-    // uploadWidget() {
-    //    // . . .
-    // }
-        return (
-            <div className="main">
-                <h1>Gallery</h1>
-                <div className="gallery">
-                    <CloudinaryContext cloudName="cloud_name">
-                        {
-                            gallery.map(data => {
-                                return (
-                                    <div className="responsive" key={data.public_id}>
-                                        <div className="img">
-                                            <a target="_blank" href={`https://res.cloudinary.com/demo/image/upload/${data.public_id}.jpg`}>
-                                                <Image publicId={data.public_id}>
-                                                    <Transformation
-                                                        crop="scale"
-                                                        width="300"
-                                                        height="200"
-                                                        dpr="auto"
-                                                        responsive_placeholder="blank"
-                                                    />
-                                                </Image>
-                                            </a>
-                                            <div className="desc">Created at {data.created_at}</div>
-                                        </div>
-                                    </div>
-                                )
-                            })
-                        }
-                    </CloudinaryContext>
-                    <div className="clearfix"></div>
-                </div>
+  return (
+    <div>
+      <h1>Gallery</h1>
+      <div className="masonry-with-columns">
+        {gallery.map((image) => (
+          <img src ={image.url} alt={image.public_id}/>
+   
+              
+        ))}
             </div>
-
+            </div>
         );
-    }
-
-
-    export default Gallery;
-
+}
+        export default Gallery;
