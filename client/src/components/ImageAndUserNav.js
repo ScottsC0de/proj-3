5b576d84064fd98e3bdeff5897cb4dca0a7bb269
Mike Johnson <mike@mapleprintservices.com>
2023-03-07 20:00:23 -0500

    conditional render users page button

--- client/src/components/ImageAndUserNav.js
+++ client/src/components/ImageAndUserNav.js
@@ -1,51 +1,29 @@
-import React, { useState } from 'react';
-import { Link, useLocation, Route } from 'react-router-dom';
-import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Link, useLocation, Route } from "react-router-dom";
+import { Navbar, Nav, Container, Modal, Tab, Button } from "react-bootstrap";
 // import AllSavedImages from "./pages/AllSavedImages";
 // import UserList from "./pages/UserList";
 
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 
 const UserNav = () => {
   // set modal display state
   const [showModal, setShowModal] = useState(false);
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const location = useLocation();
-  const handleSearchSubmit = (searchText) => {
-
-  }
-
+  // const handleSearchSubmit = (searchText) => {};
 
   return (
-    <>
-      <Navbar bg='dark' variant='dark' expand='lg'>
-        <Container fluid>
-          <Navbar.Toggle aria-controls='navbar' />
-          <Navbar.Collapse id='navbar'>
-            <Nav className='ml-auto'>
-            {location.pathname !== "/" ? <Nav.Link as={Link} to='/'>
-            See Everyone's Inspiration</Nav.Link> : ''}
-
-
-              {/* if user is logged in show saved images and users */}
-                <>
-                {location.pathname !== "/allsaved" ?
-                  <Nav.Link as={Link} to='/allsaved'>
-                     See Everyone's Inspiration
-                  </Nav.Link> : ''}
-                  {location.pathname !== "/allusers" ?
-                  <Nav.Link as={Link} to='/allusers'>
-                    See All Friends
-                  </Nav.Link> : ''}
-
-                </>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      
-    </>
+    <div className="mx-auto">
+       {/* if user is logged in show saved images and logout */}
+             { ((Auth.loggedIn()) && (location.pathname === "/allsaved")) ? 
+                  (<Link
+                    to='/allusers'
+                    className="btn btn-info btn-inline-block mx-auto"
+                    // onClick={() => handleSaveImage(photo.imageSrc)}
+                  >See images by user</Link>) : ('')
+  }
+    </div>
   );
 };
-
 export default UserNav;
--- client/src/pages/AllSavedImages.js
+++ client/src/pages/AllSavedImages.js
@@ -25,7 +25,9 @@ const AllSavedImages = () => {
 
     return (
       <>
+      <Container>
       <UserNav />
+</Container>
         <main>
           <div className="flex-row justify-center">
             <div className="col-12 col-md-10 my-3">
